33328a7581eb5b25a4ecf33e772e56dd446d1deb
Daniel Miles <daniel.t.miles@gmail.com>
2017-01-03 22:30:25 -0800

    I decided that the cards validity lists should be available to outside classes, so I changed the underscore convention to a capital letter convention

--- cards/card.py
+++ cards/card.py
@@ -1,19 +1,19 @@
 class Card(object):
-    _valid_suits = ["heart", "spade", "club", "diamond"]
-    _red_suits = ["heart", "diamond"]
-    _black_suits = ["spade", "club"]
-    _valid_values = ["Ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King"]
+    Valid_suits = ["heart", "spade", "club", "diamond"]
+    Red_suits = ["heart", "diamond"]
+    Black_suits = ["spade", "club"]
+    Valid_values = ["Ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King"]
 
     def __init__(self, suit, value):
-        if suit not in Card._valid_suits:
-            raise ValueError("Unknown suit %s, must be one of %s" % (suit, ", ".join(Card._valid_suits)))
+        if suit not in Card.Valid_suits:
+            raise ValueError("Unknown suit %s, must be one of %s" % (suit, ", ".join(Card.Valid_suits)))
         self.suit = suit
-        if value not in Card._valid_values:
-            raise ValueError("Unknown card value %s, must be one of %s", (value, ", ".join([str(x) for x in Card._valid_values])))
+        if value not in Card.Valid_values:
+            raise ValueError("Unknown card value %s, must be one of %s", (value, ", ".join([str(x) for x in Card.Valid_values])))
         self.value = value
 
         self.color = ""
-        if suit in Card._black_suits:
+        if suit in Card.Black_suits:
             self.color = "black"
-        if suit in Card._red_suits:
+        if suit in Card.Red_suits:
             self.color = "red"
